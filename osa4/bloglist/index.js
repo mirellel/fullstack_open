eecfc2b48c916fb1a8c8d1f302bb4d7a2d11c73c
mirelle01 <lybackmirelle@gmail.com>
2023-09-23 16:27:33 +0300

    teht 4.2

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,33 +1,7 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Blog = require('./models/blogs')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
