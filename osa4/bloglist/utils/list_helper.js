09217ca4edb399ffff5b1b25f539175348cd4241
mirelle01 <lybackmirelle@gmail.com>
2023-09-26 13:14:15 +0300

    4.6-4.7

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -13,8 +15,36 @@ const favouriteBlog = (blogs) => {
         : blogs.reduce((favourite, blog) => blog.likes > favourite.likes ? blog : favourite, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    const blogCount = lodash.countBy(blogs, 'author')
+    const authorKey = Object.keys(blogCount)[0]
+    const first = {
+        author: authorKey,
+        blogs: blogCount[authorKey]
+    }
+
+    const top = Object.keys(blogCount).reduce((max, key) => blogCount[key] > max.blogs ? {author: key, blogs: blogCount[key]} : max, first)
+    return top.author === undefined
+        ? 0
+        : top
+}
+
+const mostLikes = (blogs) => {
+    const authorsBlogs = lodash.groupBy(blogs, 'author')
+    const authorsLikes = lodash.mapValues(authorsBlogs, likes => lodash.sumBy(likes, 'likes'))
+    const top = lodash.maxBy(Object.keys(authorsLikes), author => authorsLikes[author])
+    return top === undefined
+        ? 0
+        : [
+            author = top,
+            likes = authorsLikes[top]
+        ]
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
